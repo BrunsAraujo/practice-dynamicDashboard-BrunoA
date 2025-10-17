5ff98d2bb71fec89f18f3e027c61ebefca6d665e
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-17 18:28:35 -0500

    Homework React Basics

---
+++ DynamicDashboard/src/App.jsx
@@ -0,0 +1,19 @@
+import Greeting from "./Components/Greeting.jsx"
+import Premium from "./Components/Premium.jsx"
+import TaskList from "./Components/TaskList.jsx"
+
+function App() {  
+
+  return (
+    <>
+      <div>
+      <Greeting/>
+      <Premium/>
+      <TaskList/>
+  
+      </div>
+    </>
+  )
+}
+
+export default App
