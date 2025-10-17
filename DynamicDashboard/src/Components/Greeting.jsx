a7825e0270fac76fcc94e84b07805ae62f1b9911
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-17 18:27:34 -0500

    Homework React Basics

---
+++ DynamicDashboard/src/Components/Greeting.jsx
@@ -0,0 +1,12 @@
+function Greeting() {
+const username = "Bruno"
+    return(
+        <header>
+            <h1> Hello, {username}!</h1>
+ 
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Greeting
